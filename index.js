12f83be104c6788016ade5d60abbc9a8ed864cfa
Olivier Rousseau-Guyot <olivierr@playtika.com>
2018-02-27 12:51:36 -0500

    adjusted for new chrome restrictions

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// absolution
 function WaitWhat() {
   throw 'Hey you\'re not supposed to use javascript';
 }
-['document', 'location'].forEach(node => {
-  for (var key in window[node]) {
-    window[node].__defineGetter__(key, WaitWhat);
-    window[node].__defineSetter__(key, WaitWhat);
+
+// objects to purge from evil
+[
+  document,
+  window
+]
+
+// ceremony
+.forEach(node => {
+  if(!node) return;
+  for (var key in node) {
+    try {
+      window[node].__defineGetter__(key, WaitWhat);
+      window[node].__defineSetter__(key, WaitWhat);
+    } catch(e) {
+      if (console) console.log(`cannot mutate ${node}:`, e);
+    }
   }
 });
